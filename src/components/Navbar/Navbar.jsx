6456cd943c8b9a5d3fe1ae9f767db3a0b79ed25e
Nithish Kumar <nithishpadma@gmail.com>
2021-08-01 09:12:37 +0530

    create navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.scss'
+
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+        <div className="navbar__logo-box">
+           <img alt='foxigo logo' className='navbar__logo' src="https://img.icons8.com/fluency/48/000000/fox.png"/>
+            <p className="navbar__title">Foxigo</p>
+        </div>
+        <div className="navbar__btn-box">
+            <button className="navbar__btn">Play again</button>
+        </div>
+    </nav>
+    )
+}
+
+export default Navbar

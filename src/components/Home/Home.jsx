fbf81d50d71c4f6e527c5449fa0922cd0cbeeded
Nithish Kumar <nithishpadma@gmail.com>
2021-08-01 09:06:04 +0530

    create navbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,9 +5,13 @@ const Home = () => {
     return(
         <main className='main'>
             <div className="main__container">
-                <nav className="main__navbar">
-                    <div className="main__navbar__logo-box">
-                        <p className="main__navbar__title">Foxigo</p>
+                <nav className="navbar">
+                    <div className="navbar__logo-box">
+                       <img alt='foxigo logo' className='navbar__logo' src="https://img.icons8.com/fluency/48/000000/fox.png"/>
+                        <p className="navbar__title">Foxigo</p>
+                    </div>
+                    <div className="navbar__btn-box">
+                        <button className="navbar__btn">Play again</button>
                     </div>
                 </nav>
             </div>

c66d2fc0085a472a4c0da1397f4fb03dd99fe101
Nithish Kumar <nithishpadma@gmail.com>
2021-08-01 09:12:08 +0530

    shift navbar to seperate component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
 import './Home.scss'
 
 const Home = () => {
     return(
         <main className='main'>
             <div className="main__container">
-                <nav className="navbar">
-                    <div className="navbar__logo-box">
-                       <img alt='foxigo logo' className='navbar__logo' src="https://img.icons8.com/fluency/48/000000/fox.png"/>
-                        <p className="navbar__title">Foxigo</p>
-                    </div>
-                    <div className="navbar__btn-box">
-                        <button className="navbar__btn">Play again</button>
-                    </div>
-                </nav>
+               <Navbar />
             </div>
         </main>
     )

8c9405820b362b660068ebf5abd96c1b0db16996
Nithish Kumar <nithishpadma@gmail.com>
2021-08-01 14:30:53 +0530

    import Card component in Team1 & Team2

--- src/components/Team/Team1.jsx
+++ src/components/Team/Team1.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Team.scss'
 
-const Team1 = () => {
+const Team1 = ({ team1 }) => {
     return(
         <div className='team team--1'>
-            Team 1
+            <p className="team__title">Team 1</p>
+            <Card team1={team1} />
         </div>
     )
 }
--- src/components/Team/Team2.jsx
+++ src/components/Team/Team2.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Card from '../Card/Card'
 import './Team.scss'
 
-const Team1 = () => {
+const Team1 = ({team2}) => {
+
     return(
         <div className='team team--2'>
-            Team 2
+            <p className="team__title">Team 2</p>
+            <Card team2={team2} />
         </div>
     )
 }
